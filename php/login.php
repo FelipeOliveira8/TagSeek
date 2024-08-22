ea594af5a7ae2ada0698532d860449ed86c3b726
FelipeOliveira8 <fc0669493@gmail.com>
2024-08-22 17:21:37 -0300

    inserção do primeiro popup na tela principal

--- php/login.php
+++ php/login.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="..\css/login.css">
+    <link rel="shortcut icon" href="../images/shorticon.png">
     <title>Login</title>
 </head>
 <body>
     <section class="s-login">
         <div class="d-login1">
-        <img class="logo" src="images/logo.png">
+        <img class="logo" src="../images/logo.png">
             <form action="index.php" class="f-login">
                 <div class="input-box">
                     <span class="icon">
@@ -37,62 +38,3 @@
 </body>
 <script src="js/showPassword.js"></script>
 </html>
-
-<?php
-
-    if(isset($_POST['email'])){
-
-        include "mysqlConnect.php";
-        
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        
-        $info = mysqli_fetch_array(mysqli_query($connection, "select user_password, user_id, user_permissions, user_name, theme_preference from users where user_email like '$email';"));
-        
-        if(empty($info)){ //No info returned thus, the user doesn't exist
-            
-            mysqli_close($connection);
-            
-            echo"<form action='../login.php' method='post' id='error'><input type='hidden' id='err' name='err' value='100'></form>";
-            echo"<script>document.getElementById('error').submit();</script>";
-            
-        }else if(!password_verify($password, $info[0])){ //Wrong password
-            
-            mysqli_close($connection);
-            
-            echo"<form action='../login.php' method='post' id='error'><input type='hidden' id='err' name='err' value='200'></form>";
-            echo"<script>document.getElementById('error').submit();</script>";
-            
-        }else{ //Login succeeded
-            
-            //Starts the user session and saves their information
-            session_start();
-            
-            $_SESSION['id'] = $info[1];
-            $_SESSION['permissions'] = $info[2];
-            $_SESSION['username'] = $info[3];
-            $_SESSION['theme'] = $info[4];
-            
-            mysqli_close($connection);
-            
-            //Makes the session last for 30 days
-            $params = session_get_cookie_params();
-            setcookie(session_name(), $_COOKIE[session_name()], time() + 60*60*24*30, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
-            
-            //Redirects user to the dashboard
-            header("Location: ../user_dashboard.php");
-            
-        }
-        
-    }else{
-        //Checks if the user is logged in
-        session_start();
-
-        if(!isset($_SESSION['username'])){
-            header('Location: ../login.php');
-        
-        }else{
-            header('Location: ../user_dashboard.php');
-        }
-    }
-?>
